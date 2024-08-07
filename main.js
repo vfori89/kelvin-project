cd4bb23eb56a6a5ca0bc0c7a3d9303c7c1154b92
Vladimir Averkov <83307115+vfori89@users.noreply.github.com>
2024-08-07 17:18:41 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+//create variable for kelvin
+const kelvin = 0;
+
+//create variable for celsius
+let celsius = kelvin - 273;
+
+//calculate the fahrenheit
+let fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+
+//print out the sentance with the fahrenheit temperature in the console
+console.log(`The temperature is ${fahrenheit} degrees Fahrengeit.`);
